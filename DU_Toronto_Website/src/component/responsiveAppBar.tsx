856ade73e4a88255134d9bcdca1ee11803f0253e
gaberielms <gabeof01@gmail.com>
2025-01-25 21:29:24 -0500

    logo routes to home

--- DU_Toronto_Website/src/component/responsiveAppBar.tsx
+++ DU_Toronto_Website/src/component/responsiveAppBar.tsx
@@ -15,11 +15,12 @@ import { ThemeProvider } from '@emotion/react';
 import { Link } from 'react-router-dom';
 
 const pages = [
-  { name: 'Home', path: '/' },
+  // { name: 'Home', path: '/' },
   { name: 'Why DU', path: '/why-Du' },
   { name: 'GreekLife 101', path: '/greek-life' },
   { name: 'Alumni', path: '/alumni' },
 ];
+
 const oldGold = '#CFB53B';
 
 const theme = createTheme({
@@ -62,14 +63,13 @@ function ResponsiveAppBar() {
       <AppBar position="sticky" sx={{ width: '100%', backgroundColor: 'white', height: '64px' }}>
         <Container maxWidth={false} sx={{ padding: 0, height: '100%' }}>
           <Toolbar disableGutters sx={{height: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'stretch' }}>
+
             {/* Logo and Logo Text */}
             <Box sx={{ display: 'flex', alignItems: 'center', padding: '0 1rem', height: '100%' }}>
               {/* <AdbIcon sx={{ mr: 1 }} /> */}
               <Typography
                 variant="h6"
                 noWrap
-                component="a"
-                href="#"
                 sx={{
                   fontFamily: 'monospace',
                   fontWeight: 700,
@@ -77,35 +77,37 @@ function ResponsiveAppBar() {
                   color: oldGold,
                   textDecoration: 'none',
                 }}
+                component={Link} to="/"
               >
                 DU
               </Typography>
             </Box>
 
-   {/* Navigation Buttons */}
-      <Box sx={{ display: { xs: 'none', md: 'flex' }, height: '100%' }}>
-        {pages.map((page) => (
-          <Button
-            key={page.name}
-            component={Link}
-          to={page.path}
-            sx={{
-            my: 0,
-            color: 'black',
-            display: 'flex', // Make the button a flex container
-            justifyContent: 'center', // Center content horizontally
-            alignItems: 'center', // Center content vertically
-            height: '100%',
-            borderRadius: 0,
-              '&:hover': {
-                backgroundColor: '#C4C4C4',
-              },
-           }}
-          >
-            {page.name}
-          </Button>
-        ))}
-      </Box>
+            {/* Navigation Buttons */}
+            <Box sx={{ display: { xs: 'none', md: 'flex' }, height: '100%' }}>
+              {pages.map((page) => (
+                <Button
+                  key={page.name}
+                  component={Link}
+                to={page.path}
+                  sx={{
+                  my: 0,
+                  color: 'black',
+                  display: 'flex', // Make the button a flex container
+                  justifyContent: 'center', // Center content horizontally
+                  alignItems: 'center', // Center content vertically
+                  height: '100%',
+                  borderRadius: 0,
+                    '&:hover': {
+                      backgroundColor: '#C4C4C4',
+                      color: 'black',
+                    },
+                }}
+                >
+                  {page.name}
+                </Button>
+              ))}
+            </Box>
 
             {/* Mobile Menu Icon */}
             <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
