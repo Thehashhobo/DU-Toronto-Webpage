1a274b9aadea63db2127e19d9e9133297d41b220
PulchritudinousHaese <micah.skazhenyuk@mail.utoronto.ca>
2026-06-23 12:15:54 -0400

    remove news

--- dutoronto/src/data/events.ts
+++ dutoronto/src/data/events.ts
@@ -8,12 +8,12 @@ export interface ChapterEvent {
 }
 
 export const upcomingEvents: ChapterEvent[] = [
-  {
-    id: "initiation-winter-2026",
-    title: "Winter Initiation",
-    date: "2026-03-28",
-    time: "6:00 PM",
-    location: "182 St. George Street, Toronto",
-    description: "Join our Chapter as we welcome the Winter 2026 Associate Member class into our brotherhood. The initiation ceremony will be followed by a celebratory dinner.",
-  }
+//   {
+//     id: "",
+//     title: "Winter Initiation",
+//     date: "2026-03-28",
+//     time: "6:00 PM",
+//     location: "182 St. George Street, Toronto",
+//     description: "Join our Chapter as we welcome the Winter 2026 Associate Member class into our brotherhood. The initiation ceremony will be followed by a celebratory dinner.",
+//   }
 ];
